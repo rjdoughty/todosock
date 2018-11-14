0ad84d85ca431d759eb4322504243a8749489c0a
DoughBoy <41695186+rjdoughty@users.noreply.github.com>
2018-11-14 00:35:51 -0500

    added heroku/mlab

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Connect to the Mongo DB using the database (will be created if it doesn't exist)
-mongoose.connect('mongodb://localhost/tasklist', { useNewUrlParser: true });
-//mongoose.connect('mongodb://todo:abc123@ds121163.mlab.com:21163/heroku_s8l0bw78', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost/tasklist', { useNewUrlParser: true });
+mongoose.connect('mongodb://todosock:abc123@ds163013.mlab.com:63013/heroku_cwbrfx0x', { useNewUrlParser: true });
 
 // Import our routes and pass it 'app' as an argument
 require('./sockets/task-sockets.js')(io);
